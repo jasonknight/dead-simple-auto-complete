352025b785561fbe7d02f63ddbd22e0be87dc7f3
Jason Martin <jasonknight@users.noreply.github.com>
2020-02-04 18:46:25 -0500

    Update autocomplete.js

--- autocomplete.js
+++ autocomplete.js
@@ -1,3 +1,31 @@
 function AutoComplete(opts) {
-
+  let self = this;
+  let default_opts = {
+    cmp: function (a,b) {
+      
+      return false;
+    },
+    render: function (results) {
+      
+    },
+  };
+  self.what = opts.what; // This is the data source
+  self.who = opts.who; // This is the target element, you need to pass the element in
+  self.cmp = opts.compare; // the comparison function
+  self.min_chars = opts.min_chars; // This is how many characters before a search is made
+  self.who.addEventListener('keyup', function (e) {
+    let v = self.who.value;
+    if ( v.length > self.min_chars ) {
+      let results = [];
+      for ( let i in self.what ) {
+        if ( self.cmp(v,self.what[i]) ) {
+          results.push(self.what[i]);
+        }
+      }
+      if ( results.length > 0 ) {
+        self.render(results); 
+      }
+    }
+  };
+  return self;
 }
